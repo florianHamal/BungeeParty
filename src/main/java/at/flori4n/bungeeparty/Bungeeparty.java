1ef5e86d690a0c4241f6e9e94b7623a68ea44720
florian <florian.hamal03@gmail.com>
2023-03-01 14:45:26 +0100

    working

--- src/main/java/at/flori4n/bungeeparty/Bungeeparty.java
+++ src/main/java/at/flori4n/bungeeparty/Bungeeparty.java
@@ -3,12 +3,16 @@ package at.flori4n.bungeeparty;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.plugin.Plugin;
 
+
 public final class Bungeeparty extends Plugin {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         ProxyServer.getInstance().getPluginManager().registerCommand(this,new PartyCommand());
+        ProxyServer.getInstance().getPluginManager().registerListener(this,new Listeners());
+
+
     }
 
     @Override
--- src/main/java/at/flori4n/bungeeparty/Listeners.java
+++ src/main/java/at/flori4n/bungeeparty/Listeners.java
@@ -1,22 +1,34 @@
 package at.flori4n.bungeeparty;
 
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.ServerConnectRequest;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
-import net.md_5.bungee.api.event.PlayerDisconnectEvent;
-import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.event.*;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
 
 public class Listeners implements Listener {
-    @EventHandler
+    /*@EventHandler
     public void onPlayerJoin(PostLoginEvent event){
         ProxiedPlayer partyleader = PartyData.getInstance().getPartyLeader();
         if (partyleader==null) return;
         event.getPlayer().connect(partyleader.getServer().getInfo());
-    }
+    }*/
     @EventHandler
     public void onPlayerDisconect(PlayerDisconnectEvent event){
         if (event.getPlayer()==PartyData.getInstance().getPartyLeader()){
             PartyData.getInstance().setPartyLeader(null);
+        }
+    }
+    @EventHandler
+    public void onServerConnected(ServerConnectedEvent event){
+        ProxiedPlayer partyLeader = PartyData.getInstance().getPartyLeader();
+        if (partyLeader==null)return;
+        if (event.getPlayer()==partyLeader){
+            ProxyServer.getInstance().getPlayers().forEach(p ->{p.connect(event.getServer().getInfo());});
+        }else {
+            event.getPlayer().connect(partyLeader.getServer().getInfo());
         }
     }
 }
