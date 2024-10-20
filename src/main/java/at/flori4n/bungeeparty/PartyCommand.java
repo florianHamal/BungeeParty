8a38ef10611a9199a207c566176c75cce4cdd06f
flori <hamflb19@htl-kaindorf.at>
2024-10-20 22:26:19 +0200

    some changes

--- src/main/java/at/flori4n/bungeeparty/PartyCommand.java
+++ src/main/java/at/flori4n/bungeeparty/PartyCommand.java
@@ -2,6 +2,8 @@ package at.flori4n.bungeeparty;
 
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.BaseComponent;
+import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
@@ -12,8 +14,23 @@ public class PartyCommand extends Command {
 
     @Override
     public void execute(CommandSender sender, String[] args) {
-        ProxiedPlayer proxiedPlayer = ProxyServer.getInstance().getPlayer(sender.getName());
-        PartyData.getInstance().setPartyLeader(proxiedPlayer);
-        ProxyServer.getInstance().getPlayers().forEach(p ->{p.connect(proxiedPlayer.getServer().getInfo());});
+        if (!sender.hasPermission("party.start")){
+            sender.sendMessage(new ComponentBuilder("you are not allowed to perform this action").create());
+            return;
+        }
+        switch (args[0]){
+            case "start":
+                ProxiedPlayer proxiedPlayer = ProxyServer.getInstance().getPlayer(sender.getName());
+                PartyData.getInstance().setPartyLeader(proxiedPlayer);
+                ProxyServer.getInstance().getPlayers().forEach(p ->{p.connect(proxiedPlayer.getServer().getInfo());});
+                sender.sendMessage(new ComponentBuilder("starting party").create());
+                break;
+            case "stop":
+                PartyData.getInstance().setPartyLeader(null);
+                sender.sendMessage(new ComponentBuilder("stopping party").create());
+                break;
+            default:
+                sender.sendMessage(new ComponentBuilder("invalidCommand").create());
+        }
     }
 }
